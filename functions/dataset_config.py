f12e5caaaa30c750f8f86718edc83da05311f500
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2025-10-19 11:32:42 -0300

    :recycle: refactor: remove remaining unused train transformation

--- functions/dataset_config.py
+++ functions/dataset_config.py
@@ -18,12 +18,6 @@ class LC25000DatasetConfig:
     ])
 
     TRAIN_TRANSFORMS = A.Compose([
-        A.RGBShift(
-            r_shift_limit=15,
-            g_shift_limit=15,
-            b_shift_limit=15,
-            p=0.5
-        ),
         A.Resize(224, 224),
         A.Normalize(
             mean=[0.485, 0.456, 0.406],
