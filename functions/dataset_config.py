efae951009d066d27cc36d5f6756fa7470e10cf8
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2025-10-19 10:15:04 -0300

    🚚 refactor: project folder structure

--- functions/dataset_config.py
+++ functions/dataset_config.py
@@ -24,19 +24,7 @@ class LC25000DatasetConfig:
             b_shift_limit=15,
             p=0.5
         ),
-        A.RandomBrightnessContrast(p=0.5),
-        A.ToGray(p=0.1),
-        A.UnsharpMask(p=0.1),
-        A.HorizontalFlip(p=0.3),
-        A.SafeRotate(limit=30),
-        A.HueSaturationValue(p=0.2),
-        A.Blur(p=0.1),
-        A.GaussNoise(p=0.1),
-        A.ISONoise(p=0.2),
-        A.Sharpen(p=0.1),
-        A.CLAHE(p=0.05),
-        A.ElasticTransform(p=0.1),
-        A.Resize(224, 224, interpolation=cv2.INTER_LANCZOS4),
+        A.Resize(224, 224),
         A.Normalize(
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]
