e4ddfdd80c4bbc13713f18b8e7fcf8e25da9de1b
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2026-04-01 08:35:07 -0300

    :sparkles: feat: new notebook - alexnet with 50 epochs

--- functions/imports.py
+++ functions/imports.py
@@ -22,3 +22,4 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from tabulate import tabulate
 import seaborn as sns
+import copy
