fbe46bbdf567a08d9e5f82d9b113c9ad3ca7f1f8
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2026-04-01 08:10:29 -0300

    :heavy_plus_sign: feat: add imports from train and test

--- functions/imports.py
+++ functions/imports.py
@@ -19,3 +19,6 @@ import os
 import datetime
 import torchvision
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from tabulate import tabulate
+import seaborn as sns
